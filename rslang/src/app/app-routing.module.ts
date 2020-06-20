da07b37aa89d3079368cd3ac0cca3b76b00b606d
annaStoma <43847536+annaStoma@users.noreply.github.com>
2020-06-20 14:34:51 +0300

    Revert "002: Main page"

--- rslang/src/app/app-routing.module.ts
+++ rslang/src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MainPageComponent } from './modules/main-page/main-page.component';
 
-const routes: Routes = [{ path: '', component: MainPageComponent }];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- rslang/src/app/app.module.ts
+++ rslang/src/app/app.module.ts
@@ -4,40 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainPageComponent } from './modules/main-page/main-page.component';
-import { GameComponent } from './modules/main-page/components/game/game.component';
-import { DictionariesComponent } from './modules/main-page/components/dictionaries/dictionaries.component';
-import { MatButtonModule } from '@angular/material/button';
-import { StatisticCardComponent } from './modules/main-page/components/statistic-card/statistic-card.component';
-import { MatTableModule } from '@angular/material/table';
-import { PromoComponent } from './modules/main-page/components/promo/promo.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainPageComponent,
-    GameComponent,
-    DictionariesComponent,
-    StatisticCardComponent,
-    PromoComponent,
-    HeaderComponent,
-    FooterComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatTableModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatMenuModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
