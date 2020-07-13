3e307989cf4f2d3005024ab8267d6efde1e72fe8
SoluyanovKonstantin <dr.chtec@yandex.ru>
2020-07-13 20:17:51 +0400

    refactor: add data types in words.service

--- rslang/src/app/modules/mini-games/modules/english-puzzle/component/english-puzzle/words.service.ts
+++ rslang/src/app/modules/mini-games/modules/english-puzzle/component/english-puzzle/words.service.ts
@@ -2,15 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../../../../../../shared/services/api.service';
 import { Config } from '../../../../../../common/config';
+import { Observable } from 'rxjs';
+
+interface Statistic {
+  errorRatePercent,
+  totalGamesCompleted
+}
 
 @Injectable({
   providedIn: 'root',
 })
-export class WordsService {
+export class WordsService  {
   constructor(private http: HttpClient, private apiService: ApiService) { }
   page = 1;
   level = 1;
-  getWords() {
+  getWords() : Observable<Object> {
     return this.http.get(
       `https://api-rslang.herokuapp.com/words?page=${
       Math.floor((this.page - 1) / 2)
@@ -18,10 +24,9 @@ export class WordsService {
     );
   }
 
-
-  setUserStatistic(rightWordsCount, wrongWordsCount) {
-    let oldStatistic;
-    let commonRating;
+  setUserStatistic(rightWordsCount, wrongWordsCount) : void {
+    let oldStatistic : Statistic;
+    let commonRating : number;
     this.apiService.getUserStatistics().subscribe(res => {
       oldStatistic = res.optional ? res.optional["english-puzzle"] : { errorRatePercent : 0, totalGamesCompleted: 0 };
       commonRating = (wrongWordsCount / (rightWordsCount + wrongWordsCount)) + (oldStatistic.errorRatePercent / 100);
