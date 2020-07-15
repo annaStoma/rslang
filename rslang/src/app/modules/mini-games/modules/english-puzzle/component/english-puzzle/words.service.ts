4bec4dcfb95692be5b1b4ae2af0f8105a6a08b2d
NexGenUA <mr.rinus@gmail.com>
2020-07-15 15:29:37 +0300

    fix: update stats english-puzzle mini-game

--- rslang/src/app/modules/mini-games/modules/english-puzzle/component/english-puzzle/words.service.ts
+++ rslang/src/app/modules/mini-games/modules/english-puzzle/component/english-puzzle/words.service.ts
@@ -1,44 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../../../../../../shared/services/api.service';
-import { Config } from '../../../../../../common/config';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 
 interface Statistic {
-  errorRatePercent,
-  totalGamesCompleted
+  errorRatePercent;
+  totalGamesCompleted;
 }
 
 @Injectable({
   providedIn: 'root',
 })
-export class WordsService  {
-  constructor(private http: HttpClient, private apiService: ApiService) { }
+export class WordsService {
+  constructor(private http: HttpClient, private apiService: ApiService) {
+  }
+
   page = 1;
   level = 1;
-  getWords() : Observable<Object> {
+
+  getWords(): Observable<object> {
     return this.http.get(
       `https://api-rslang.herokuapp.com/words?page=${
-      Math.floor((this.page - 1) / 2)
+        Math.floor((this.page - 1) / 2)
       }&group=${this.level - 1}`
     );
   }
 
-  setUserStatistic(rightWordsCount, wrongWordsCount) : void {
-    let oldStatistic : Statistic;
-    let commonRating : number;
+  setUserStatistic(rightWordsCount, wrongWordsCount): void {
+    let oldStatistic: Statistic;
+    let commonRating: number;
     this.apiService.getUserStatistics().subscribe(res => {
-      oldStatistic = res.optional ? res.optional["english-puzzle"] : { errorRatePercent : 0, totalGamesCompleted: 0 };
+      oldStatistic = res.optional ? res.optional['english-puzzle'] : {errorRatePercent: 0, totalGamesCompleted: 0};
       commonRating = (wrongWordsCount / (rightWordsCount + wrongWordsCount)) + (oldStatistic.errorRatePercent / 100);
       commonRating = oldStatistic.totalGamesCompleted > 0 ? commonRating / 2 : commonRating;
       this.apiService.updateUserStatistics({
         optional: {
-          "english-puzzle": {
+          'english-puzzle': {
             errorRatePercent: commonRating * 100,
             totalGamesCompleted: oldStatistic.totalGamesCompleted + 1,
           }
         }
-      })
+      }).pipe(take(1)).subscribe();
     });
   }
 }
