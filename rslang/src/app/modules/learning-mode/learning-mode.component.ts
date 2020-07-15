a60a21b30ace9dca01f091b1696fd22e10b1c2fa
Hanna Stoma <annstoma9@gmail.com>
2020-07-15 22:34:51 +0300

    fix

--- rslang/src/app/modules/learning-mode/learning-mode.component.ts
+++ rslang/src/app/modules/learning-mode/learning-mode.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 import { ApiService } from '../../shared/services/api.service';
@@ -12,11 +12,6 @@ import { UsersWords, Word } from '../../shared/interfaces';
 
 export class LearningModeComponent implements OnInit {
   wordsToLearn: Array<Word>;
-  img: Array<string> = [
-    'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg',
-    'https://static.toiimg.com/thumb/72975551.cms?width=680&height=512&imgsize=881753',
-    'https://image.shutterstock.com/image-photo/mountains-during-sunset-beautiful-natural-260nw-407021107.jpg'
-  ];
   public customOptions: OwlOptions = {
     loop: true,
     center: true,
@@ -104,6 +99,19 @@ export class LearningModeComponent implements OnInit {
       difficulty: 'hard',
       optional: {
         hard: true
+      }
+    };
+    this.apiService.createUserWordByWordId(id, body).subscribe(_ => {
+      return this.apiService.updateUserWordByWordId(id, body).subscribe(data => data);
+    });
+  }
+
+  addToLearned(id: string): void {
+    this.wordsToLearn = this.wordsToLearn.filter(n => n._id !== id);
+    const body: UsersWords = {
+      difficulty: 'easy',
+      optional: {
+        learned: true
       }
     };
     this.apiService.createUserWordByWordId(id, body).subscribe(_ => {
